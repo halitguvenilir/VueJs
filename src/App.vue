002e5fdb6007efe2c400099fc0ec46bf4532142e
Halit Güvenilir <halitguvenilir@gmail.com>
2018-07-14 12:02:53 +0300

    Starwars api fetch method added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div id="app">
     <div class="title">
         <h3>{{ title }}</h3>
-        <h4>{{ user }}</h4>
+        <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
     </div>
 </div>
 </template>
@@ -12,8 +12,16 @@ export default {
     name: 'app',
     data() {
         return {
-            title: 'Hello Darth Vader',
-            user: 'Halit GÜVENİLİR'
+            title: 'Hello Darth Vader'
+        }
+    },
+    methods: {
+        fetchCharacter() {
+            fetch('https://swapi.co/api/people', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
         }
     }
 }
@@ -24,13 +32,5 @@ export default {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-
-.title {
-    display: flex;
-    justify-content: space-between;
-    color: red;
-    background: #000;
-    padding: 10px;
 }
 </style>
