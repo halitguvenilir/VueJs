b406841227f8c51ea939194747b7feaa05a1b9dc
Halit Güvenilir <halitguvenilir@gmail.com>
2018-07-14 11:13:03 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="app">
+    <div class="title">
+        <h3>{{ title }}</h3>
+        <h4>{{ user }}</h4>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Hello Darth Vader',
+            user: 'Halit GÜVENİLİR'
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.title {
+    display: flex;
+    justify-content: space-between;
+    color: red;
+    background: #000;
+    padding: 10px;
+}
+</style>
