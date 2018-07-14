3d5e4b9200dfd57d6722497f75300217adad138b
Halit Güvenilir <halitguvenilir@gmail.com>
2018-07-14 14:35:18 +0300

    Starwars api click button changed content

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 <div id="app">
     <div class="container">
         <h3>{{ title }}</h3>
-        <Character/>
+        <div class="col-md-12">
+            <Character
+                v-for="(id, index) in initial_ids"
+                :id="id"
+                key="index"
+            />
+        </div>
     </div>
 </div>
 </template>
@@ -14,7 +20,8 @@ export default {
     name: 'app',
     data() {
         return {
-            title: 'Hello Darth Vader'
+            title: 'Hello Darth Vader',
+            initial_ids: [1]
         }
     },
     components: {
